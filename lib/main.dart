d91fc11203a329a64fcdd68e56ae576ec026c301
Sqqur000 <s2036164@gmail.com>
2026-09-14 14:30:53 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+<<<<<<< HEAD
 import 'package:final_project/screens/home_screen.dart';
 import 'package:final_project/screens/splash_screen.dart';
 import 'package:final_project/screens/login_screen.dart';
 import 'package:final_project/screens/reset_password_screen.dart';
+=======
+import 'package:final_project/screens/login_screen.dart';
+>>>>>>> 3665661 (test)
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
